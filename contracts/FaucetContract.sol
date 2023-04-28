5186644997523e9cbd28b08d6e19c8e6968fb72a
dayo-odesola <dayoodesola2001@gmail.com>
2023-04-28 20:05:45 +0100

    send transaction

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,5 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+
+  //this is a special function 
+  //it's called when you make a tx that dosen't specify
+
   receive() external payable{}
 }
