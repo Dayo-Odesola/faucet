cad1ec66bbc696bf95a74f4e50a64b00fdbce4d5
dayo-odesola <dayoodesola2001@gmail.com>
2023-04-29 23:42:01 +0100

    get funders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-  address[] public funders;
+  address[] private funders;
 
   receive() external payable{}
 
@@ -12,5 +12,18 @@ contract Faucet {
     funders.push(msg.sender);
   }
 
+  function getAllFunders() public view returns (address[] memory){
+    return funders;
+  }
+
+  function getFunderAtIndex(uint8 index) external view  returns(address) {
+    address[] memory _funders = getAllFunders();
+    return _funders[index];
+  }
+
   
 }
+
+
+// const instance = Faucet.deployed()
+// instance.addFunds({from: accounts[0], value: "2"})
