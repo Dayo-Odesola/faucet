23fcb1d06dc80cbf66838813870e3f19af1acc2d
dayo-odesola <dayoodesola2001@gmail.com>
2023-05-03 20:58:10 +0100

    web3 integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import "./App.css"
+import { useState } from "react"
 import { useEffect } from "react"
 import Web3 from "web3"
 
 function App() {
 
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
   useEffect(() => {
     const loadProvider = async () => {
-      // with metamask we have an access to window.ehtereum to window.web3
-      // metamask injects a global API to websites
-      // this API allows websites to request users, accounts, read data to blockchain
-      // sign messages and transactions
+      // the latest metamask
+      let provider = null;
+      if(window.ethereum) {
+        provider.ethereum
 
-      console.log(window.web3)
-      console.log(window.ehtereum)
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access")
+          
+        }
+      }
+      // in case of lagacy metamasks
+      else if(window.web3) {
+        provider = window.web3.currentProvider
+      }
+      // for ganache
+      else if (!process.env.production) {
+         provider = new Web3.providers.HTTPPrivider("http://localhost:7545")
+      }
     }
+
+    setWeb3Api({
+      web3: new Web3(Web3.provider),
+      provider
+    })
 
     loadProvider()
   }, [])
@@ -25,16 +49,6 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance : <strong>10</strong> ETH
           </div>
-          <button
-           className="btn mr-2"
-           onClick={async () => {
-            const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
-            console.log(accounts)
-           }}
-            
-          >
-            Enable Ehtereum
-          </button>
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
