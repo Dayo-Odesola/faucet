7885d94e056669fae8768c49754902d2374ce828
dayo-odesola <dayoodesola2001@gmail.com>
2023-05-03 20:20:10 +0100

    enable ethereum

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import "./App.css"
+import { useEffect } from "react"
+import Web3 from "web3"
 
 function App() {
-  return (
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ehtereum to window.web3
+      // metamask injects a global API to websites
+      // this API allows websites to request users, accounts, read data to blockchain
+      // sign messages and transactions
+
+      console.log(window.web3)
+      console.log(window.ehtereum)
+    }
+
+    loadProvider()
+  }, [])
+  
+  return (  
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
           <div className="balance-view is-size-2">
             Current Balance : <strong>10</strong> ETH
           </div>
+          <button
+           className="btn mr-2"
+           onClick={async () => {
+            const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
+            console.log(accounts)
+           }}
+            
+          >
+            Enable Ehtereum
+          </button>
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
